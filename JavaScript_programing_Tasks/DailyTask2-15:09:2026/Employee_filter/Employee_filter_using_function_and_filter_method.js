a41a5ff70c0fc11673af25ad7d53db1b82cb59ba
Tamil Selvan S <tamil.selvan.75457@gmail.com>
2026-09-16 13:54:05 +0530

    Add employee filter function to filter by salary

---
+++ JavaScript_programing_Tasks/DailyTask2-15:09:2026/Employee_filter/Employee_filter_using_function_and_filter_method.js
@@ -0,0 +1,11 @@
+let employees = [
+    { name: "Arun", salary: 45000 },
+    { name: "Kumar", salary: 60000 },
+    { name: "Priya", salary: 75000 },
+    { name: "Ravi", salary: 50000 },
+    { name: "Vijay", salary: 55000 }
+];
+let result=employees.filter(function(employees){
+  return employees.salary>50000;
+});
+console.log(result);
